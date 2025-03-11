4e9b81e028f1cbe67672f86505330f08ac280e91
lasysz <laistenorio7@gmail.com>
2025-03-11 09:40:06 -0300

    feat:Projeto Devops

---
+++ ApiPiaget/Models/Aluno.cs
@@ -0,0 +1,10 @@
+﻿namespace ApiPiaget.Models
+{
+    public class Aluno
+    {
+        public int Id { get; set; }
+        public string? Nome { get; set; }
+        public string? Rgm { get; set; }
+        public decimal Nota { get; set; }
+    }
+}
